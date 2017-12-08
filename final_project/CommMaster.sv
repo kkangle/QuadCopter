985f432c8c6599206678cc67f1244fa9d74f005e
kkangle <32599769+kkangle@users.noreply.github.com>
2017-12-07 21:24:06 -0600

    added CommMaser v2

--- final_project/CommMaster.sv
+++ final_project/CommMaster.sv
@@ -1,149 +1,121 @@
+module commaster(clk, rst_n, cmd, snd_cmd, data, TX, RX, frm_snt, rx_data, rx_rdy);
 
-module CommMaster(snd_cmd,data,cmd,clk,rst_n,RX,TX,frm_snt,clr_frm_snt_out,rx_data,rx_rdy);
-//yudong chen 
-	input snd_cmd;
-	input [15:0]data;
-	input [7:0] cmd;
-	input clk, rst_n;
-	input RX;
-	input clr_frm_snt_out;
-	
-	output logic  TX;
-	output logic  frm_snt;
-	output logic  rx_rdy;
-	output logic  [7:0]rx_data;
-	
-	logic [7:0]tx_data;
-	logic tx_done;
-	logic [7:0]highdata,lowdata;
-	logic trmt;
-	logic [1:0]sel;
-	logic set_frm_snt,clr_frm_snt;
-	
-	
-	UART iUART(.clk(clk),.rst_n(rst_n),.RX(RX),.TX(TX),.rx_rdy(rx_rdy),.clr_rx_rdy(),.rx_data(rx_data),.trmt(trmt),.tx_data(tx_data),.tx_done(tx_done));
+input clk,rst_n,snd_cmd, RX;
+input [7:0] cmd;
+input [15:0] data;
+output TX, frm_snt, rx_rdy;
+output [7:0] rx_data;
+logic [1:0] sel;
+logic frm_snt, tx_done, rx_rdy, clr_rx_rdy, trmt, tx_dong, set_cmplt, clr_cmplt;
 
-	
-	always@(posedge clk)begin
-		if(snd_cmd==1)begin
-			highdata<=data[15:8];
-			lowdata<=data[7:0];
-		end
-			//else hold 
-	end
-	
-	
-	always_comb begin
-		if(sel[1]==0 && sel[0]==0)begin
-			tx_data=cmd;
-		end
-		else if (sel[1]==0 && sel[0]==1)begin
-			tx_data =highdata;
-		end
-		else begin
-			tx_data=lowdata;
-		end
+reg [7:0] flop1;
+reg [7:0] flop2 ;
+reg [7:0] tx_data, rx_data;
 
-	end
-	
-	
-	
-	always@(posedge clk,negedge rst_n)begin
-		if(rst_n==0)begin
-			frm_snt<=0;
+UART uart_trasiver(.clk(clk),.rst_n(rst_n),.TX(TX),.RX(RX),.rx_rdy(rx_rdy),.clr_rx_rdy(clr_rx_rdy),
+					.rx_data(rx_data),.trmt(trmt),.tx_data(tx_data),.tx_done(tx_done));
+typedef enum reg[1:0] {idle, waith, waitm, waitl} state_t;
+state_t state, nxt_state; 
+
+// flop for upper 8bits data
+always@(posedge clk, negedge rst_n) begin
+	if(!rst_n)
+		flop1 <= 8'h00;
+	else if(snd_cmd)
+		flop1 <= data[15:8];
+	else
+		flop2 <= flop1;
+end
+
+// flop for lower 8bits data
+always@(posedge clk, negedge rst_n) begin
+
+	if(!rst_n)
+		flop2 <= 8'h00;
+	else if(snd_cmd)
+		flop2 <= data[7:0];
+	else 
+		flop2 <= flop2;
+end
+
+// flop for frm_snt output 
+always@(posedge clk, negedge rst_n) begin
+	if(!rst_n)
+		frm_snt <= 0;
+	else if(clr_cmplt)
+		frm_snt <= 0;
+	else if(set_cmplt)
+		frm_snt <= 1;
+	else 
+		frm_snt <= 0;
+end
+
+// txdata select
+assign tx_data = (sel == 2'b10)? cmd:
+				 (sel == 2'b01)? flop1:
+				 flop2;
+				
+				  
+			  
+always@(posedge clk, negedge rst_n)begin 
+	if(!rst_n)
+		state <= idle;
+	else
+		state <= nxt_state;
+end
+
+
+
+always_comb begin
+	trmt = 0;
+	set_cmplt = 0;
+	clr_cmplt = 0;
+	nxt_state = idle;
+	sel = 2'b10;
+	case(state)
+		idle: begin
+			if(snd_cmd) begin
+				trmt = 1;
+				clr_cmplt = 1;
+				nxt_state = waith;
+				sel = 2'b10;
+			end
+			else
+				nxt_state = idle;
 		end
-		else if (set_frm_snt==1)begin
-			frm_snt<=1;
-		end
-		else if (clr_frm_snt==1)begin
-			frm_snt<=0;
-		end
-		else if (clr_frm_snt_out==1)begin
-			frm_snt<=0;
-		end
-			//else hold 
-	end
 		
-//-----------------sm--------------//
-	typedef enum reg[1:0] {IDLE,CMD,HIGH,LOW}state_t;
-	state_t state,nextstate;
-
-	//state transition 
-	always_ff@(posedge clk,negedge rst_n)begin
-		if(rst_n==0)begin
-			state<=IDLE;
+		waith: begin
+			if(tx_done) begin
+				sel = 2'b01;
+				trmt = 1;
+				nxt_state = waitm;
+			end
+			else 
+				 nxt_state = waith;
+				 
 		end
-		else begin
-			state<=nextstate;
-		end
-	end
-
-		
-	always_comb begin
-		sel=2'b00;
-		nextstate=IDLE;
-		trmt=0;
-		clr_frm_snt=0;
-		set_frm_snt=0;
-		case(state)
-		
-			IDLE:begin
-				if(snd_cmd==1)begin
-					$display("idle to cmd");
-					sel=2'b00;
-					nextstate=CMD;
-					trmt = 1;
-					clr_frm_snt=1;
-				end
 			
+		waitm: begin 
+			if(tx_done) begin
+				sel = 2'b00;
+				trmt = 1;
+				nxt_state = waitl;
 			end
-			CMD:begin
-			    sel = 2'b01;//////////////////////////////////////////////////////////////good 
-				if(tx_done==1)begin
-					nextstate=HIGH;
-					trmt=1;
-				end
-				else begin
-					$display("cmd");
-					nextstate=CMD;
-				end
-			end
-			HIGH:begin
-				sel=2'b10;
-				if(tx_done==1)begin
-					trmt=1;
-					nextstate=LOW;
-				end
-				else begin
-					$display("high");
-					nextstate=HIGH;
-				end
-
-			end
-			LOW:begin
-				if(tx_done==1)begin
-					//sel=2'b00;
-					nextstate=IDLE;
-					set_frm_snt=1;
-				end
-				else begin
-					//sel=2'b10;
-					nextstate=LOW;
-				end
-			
-			end
-			
-			default:begin
-				nextstate=IDLE;
-			end
-		endcase
-	
-	end
+			else
+				nxt_state = waitm;
+		end
 		
-	
-	
-	
-	
+		waitl: begin 
+			if(tx_done) begin
+				set_cmplt = 1;
+				nxt_state = idle;
+			end
+			else
+				nxt_state = waitl;
+		end
+	endcase
+end
 
-endmodule 
-	
+
+
+endmodule
